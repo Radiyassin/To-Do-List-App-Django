2639cbea52f90a8d7547a75148b91a564693abb6
Yassine Radi <yassinradiz10@gmail.com>
2025-03-01 15:24:16 +0100

    yy

--- todolistApp/todo/views.py
+++ todolistApp/todo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Working')
+
+
+
